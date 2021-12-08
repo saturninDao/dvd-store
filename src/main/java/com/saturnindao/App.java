0d3e43f893c9d750970ff670a2a83faabf1a8cf8
Saturnin Magnoudéwa Dao <saturnindao@gmail.com>
2021-12-08 14:59:25 +0000

    Exercice 6 Exploter le conteneur léger Spring

--- src/main/java/com/saturnindao/App.java
+++ src/main/java/com/saturnindao/App.java
@@ -1,8 +1,8 @@
 package com.saturnindao;
 
 import com.saturnindao.controller.MovieController;
-import com.saturnindao.repository.FileMovieRepository;
-import com.saturnindao.service.DefaultMovieService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -12,11 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        MovieController movieController = new MovieController();
-        FileMovieRepository movieRepository=new FileMovieRepository();
-        DefaultMovieService movieService=new DefaultMovieService();
-        movieController.setMovieService(movieService);
-        movieService.setMovieRepository(movieRepository);
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        MovieController movieController=context.getBean(MovieController.class);
+
         movieController.addUsingConsole();
     }
 }
