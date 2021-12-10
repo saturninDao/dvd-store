f5ebbb0819d65d1f6188320065d6bda4577761af
Saturnin Magnoudéwa Dao <saturnindao@gmail.com>
2021-12-10 09:34:57 +0000

    Exercice 14 Passer à Spring boot

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,10 +1,6 @@
 
-v
-Fsrc/main/java/com/saturnindao/repository/MovieRepositoryInterface.java,e\3\e35e04d50cec0add2dcd0d407e0de462fec87a7b
 _
 /src/main/java/com/saturnindao/entity/Movie.java,5\c\5c77b1c9018a30f3cd2ae2a9da918394f7ace8ea
-7
-pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 G
 {$movies.file.location},a\b\ab3290b1915fa58ce8c87d29236404ad924c9ee8
 F
--- src/main/java/com/saturnindao/App.java
+++ src/main/java/com/saturnindao/App.java
@@ -1,21 +1,23 @@
 package com.saturnindao;
 
 import com.saturnindao.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@ComponentScan(basePackages = {"com.saturnindao.repository.file","com.saturnindao.service","com.saturnindao.controller"})
-@PropertySource("classpath:application.properties")
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
+       // ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         MovieController movieController=context.getBean(MovieController.class);
 
         movieController.addUsingConsole();
