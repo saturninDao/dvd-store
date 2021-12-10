d6a07e79c82cbb7f5d26e566ca512aea54454957
Saturnin Magnoudéwa Dao <saturnindao@gmail.com>
2021-12-10 08:47:22 +0000

    Exercice 13 Déplacer les annotations vers la classe éxécutable

--- src/main/java/com/saturnindao/App.java
+++ src/main/java/com/saturnindao/App.java
@@ -3,17 +3,19 @@ package com.saturnindao;
 import com.saturnindao.controller.MovieController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-/**
- * Hello world!
- *
- */
+@Configuration
+@ComponentScan(basePackages = {"com.saturnindao.repository.file","com.saturnindao.service","com.saturnindao.controller"})
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         MovieController movieController=context.getBean(MovieController.class);
 
         movieController.addUsingConsole();
